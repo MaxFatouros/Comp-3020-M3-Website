04ac29670c44c54d5515db70b19e487c781100ab
Max Fatouros <max.fatouros@gmail.com>
2020-11-23 13:57:53 -0600

    Restaurants page

---
+++ restaurants.js
@@ -0,0 +1,44 @@
+const allRestaurants = [
+  {
+    name: "East Indian",
+    healthLevel: 3.0,
+    expenseRating: 4.0,
+    distance: 5,
+    id:1
+  },
+  {
+    name: "Holy spice",
+    healthLevel: 3.5,
+    expenseRating: 4.5,
+    distance: 5,
+    id:2
+  },
+  {
+    name: "East Indian",
+    healthLevel: 5.0,
+    expenseRating: 4.0,
+    distance: 7,
+    id:3
+  },
+  {
+    name: "East Indian",
+    healthLevel: 2.0,
+    expenseRating: 4.0,
+    distance: 3,
+    id:4
+  },
+  {
+    name: "East Indian",
+    healthLevel: 5.0,
+    expenseRating: 4.5,
+    distance: 1,
+    id:5
+  },
+  {
+    name: "East Indian",
+    healthLevel: 1.0,
+    expenseRating: 5.0,
+    distance: 10,
+    id:6
+  },
+];
