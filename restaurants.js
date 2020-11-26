79063243fd92cb739bf085ecbe7f75995586338a
jianzhiWang10086 <74884772+jianzhiWang10086@users.noreply.github.com>
2020-11-25 22:51:15 -0600

    Update restaurants.js

--- restaurants.js
+++ restaurants.js
@@ -1,44 +1,50 @@
 const allRestaurants = [
   {
-    name: "East Indian",
+    name: "Burger Joint",
     healthLevel: 3.0,
     expenseRating: 4.0,
     distance: 5,
-    id:1
+    id:1,
+	image: "Images/Burger%20Joint.jpg"
   },
   {
-    name: "Holy spice",
+    name: "Mexican Place",
     healthLevel: 3.5,
     expenseRating: 4.5,
     distance: 5,
-    id:2
+    id:2,
+	image: "Images/Mexican%20Place.jpg"
   },
   {
-    name: "East Indian",
+    name: "Salads Bar",
     healthLevel: 5.0,
     expenseRating: 4.0,
     distance: 7,
-    id:3
+    id:3,
+	image: "Images/Salads%20Bar.jpg"
   },
   {
-    name: "East Indian",
+    name: "Pizza Joint",
     healthLevel: 2.0,
     expenseRating: 4.0,
     distance: 3,
-    id:4
+    id:4,
+	image: "Images/Pizza%20Joint.jpg"
   },
   {
     name: "East Indian",
     healthLevel: 5.0,
     expenseRating: 4.5,
-    distance: 1,
-    id:5
+    distance: 11,
+    id:5,
+	image: "Images/East%20Indian.webp"
   },
   {
     name: "East Indian",
     healthLevel: 1.0,
     expenseRating: 5.0,
     distance: 10,
-    id:6
+    id:6,
+	image: "Images/East%20Indian.webp"
   },
 ];
